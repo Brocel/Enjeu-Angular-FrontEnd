b6eff1953ba4f7a34d9ff5d21d6ebf7a9aa54c34
Brocel <geenens.prof@gmail.com>
2021-06-30 16:23:09 -0300

    edit user model

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,2 +1,41 @@
 export class User {
+
+  readonly id: number;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  age: number;
+  country: string;
+  state: string;
+  city: string;
+  password: string;
+  role: Array<"USER"|"ADMMIN">; // rôles
+
+  constructor( options: {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    userName?: string;
+    email?: string;
+    age?: number;
+    country?: string;
+    state?: string;
+    city?: string;
+    password?: string;
+    role?: Array<"USER"|"ADMMIN">;
+  } = {}) {
+    this.id = options.id || 0;
+    this.firstName = options.firstName || '';
+    this.lastName = options.lastName || '';
+    this.userName = options.userName || '';
+    this.email = options.email || '';
+    this.age = options.age || 0;
+    this.country = options.country || '';
+    this.state = options.state || '';
+    this.city = options.city || '';
+    this.password = options.password || '';
+    this.role = options.role || ["USER"];
+  }
+
 }
