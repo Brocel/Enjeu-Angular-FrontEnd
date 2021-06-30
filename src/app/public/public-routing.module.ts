bf6c7b2495e0b8833c796734fb80134e14a7d339
Brocel <geenens.prof@gmail.com>
2021-06-30 17:01:43 -0300

    create welcome component

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from '@public/welcome/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent }, // matching welcome route with welcome component
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/public/welcome/welcome.module.ts
+++ src/app/public/welcome/welcome.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeBannerComponent } from './welcome-banner/welcome-banner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WelcomeComponent,
+    WelcomeBannerComponent
+  ],
   imports: [
     SharedModule
   ]
