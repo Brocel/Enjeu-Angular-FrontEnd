d00d865a01e769837e7a74091957b42dcd275850
Brocel <geenens.prof@gmail.com>
2021-06-30 12:18:11 -0300

    import shared module in all public modules + config @app, @core, etc in tsconfig

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { WelcomeModule } from './welcome/welcome.module';
@@ -10,7 +10,7 @@ import { RegisterModule } from './register/register.module';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    SharedModule,
     PublicRoutingModule,
     WelcomeModule,
     LoginModule,
