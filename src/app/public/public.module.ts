421431d7f55e82330ef8dcb9452dc2c430e7e970
Brocel <geenens.prof@gmail.com>
2021-06-30 12:00:24 -0300

    create public modules : welcome, login, register

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { WelcomeModule } from './welcome/welcome.module';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    WelcomeModule,
+    LoginModule,
+    RegisterModule
   ]
 })
 export class PublicModule { }
