c8928e3b802041b8f3c57e94940148c58040f7eb
Brocel <geenens.prof@gmail.com>
2021-06-30 16:45:54 -0300

    edit app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' } //welcome = route par défaut
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
