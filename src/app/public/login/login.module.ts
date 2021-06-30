51fa3fe409cc62c2369e25186d3807a81027cd5a
Brocel <geenens.prof@gmail.com>
2021-06-30 17:05:26 -0300

    create login & register components

--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     SharedModule
   ]
