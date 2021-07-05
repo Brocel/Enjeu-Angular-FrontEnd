e14a245eb586dc86310f59a7d058df1abb1ae8c0
Brocel <geenens.prof@gmail.com>
2021-07-05 17:05:59 -0300

    edit styles

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from '@public/welcome/welcome/welcome.component';
+import { LoginComponent } from '@public/login/login/login.component';
+import { RegisterComponent } from '@public/register/register/register.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent } // matching welcome route with welcome component
+  { path: 'welcome', component: WelcomeComponent }, // matching welcome route with welcome component
+  { path: 'login', component:LoginComponent},
+  { path: 'register', component:RegisterComponent}
 ];
 
 @NgModule({
